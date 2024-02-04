fba5c6702d512af626461dc4816b7723b0678294
sonoMichael <zhaomichael004@gmail.com>
2024-02-04 02:59:12 +0100

    Added temp jobs

--- CareersWebsite/views.py
+++ CareersWebsite/views.py
@@ -1,9 +1,30 @@
-from asyncio import constants
 from flask import Flask, render_template, jsonify
 from CareersWebsite import app
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Milan, Italy',
+        'salary': '2,000$'
+    },
+    {
+        'id': 2,
+        'title': 'Frontend Engineer',
+        'location': 'Venice, Italy',
+        'salary': '3,000$'
+    },
+    {
+        'id': 3,
+        'title': 'Backend Engineer',
+        'location': 'Rome, Italy',
+        'salary': '4,000$'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
+
 def home():
     return render_template("index.html",
                            company_name = 'Mele',
@@ -12,28 +33,3 @@ def home():
 @app.route("/api/jobs")
 def jobs():
     return jsonify(JOBS)
-
-"""
-Don't know how to add a temp array
-
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Milan, Italy',
-        'salary': '2,000€'
-    },
-    {
-        'id': 2,
-        'title': 'Frontend Engineer',
-        'location': 'Venice, Italy',
-        'salary': '3,000€'
-    },
-    {
-        'id': 3,
-        'title': 'Backend Engineer',
-        'location': 'Rome, Italy',
-        'salary': '4,000€'
-    }
-]
-"""
